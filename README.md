e26e89b6e59db0697855bb824493b69665acfff0
distortednet <distortednetworks@gmail.com>
2014-10-07 01:39:19 -0500

    Backup system added
    - Feature: Added bookmark Import/Export system
    - UI: changed footer to better fit info
    - Misc: Made version number auto update
    - Feature: Added Changelog

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 dn-bookmark
 ===========
 
-A bookmark replacement system for Twitch
+A bookmark replacement system for Twitch.
+
+Download: https://chrome.google.com/webstore/detail/dn-bookmark/dcdjgcndpogpklnomjfpncdcjnafnpaa
+
+V1.0.2
+
+- Feature: Added bookmark Import/Export system
+- UI: changed footer to better fit info
+- Misc: Made version number auto update
+- Feature: Added Changelog
+V1.0.1
+
+- Initial build.
+- Bug fix: clicking on the wrong div class caused bookmarks to delete on accident.
--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 $( document ).ready(function() {
+
+function getVersion() {
+    var details = chrome.app.getDetails();
+    return details.version;
+}
+$(".version").html('<a href="help.html" target="_Blank">Help</a> | <a href="Backup.html" target="_Blank">Backup</a> | <a href="Restore.html" target="_Blank">Restore</a> | <a href="Changelog.html" target="_Blank">Changelog</a> | v'+getVersion());
 	chrome.storage.sync.get('Data', function(data) {
+	$('.ImportSubmit').click(function(formevent) {
+		formevent.preventDefault();
+		var DecodedData = JSON.parse(atob($('.EncodedData').val()));
+		chrome.storage.sync.get(function(cfg) {
+			cfg["Data"] = DecodedData;
+			chrome.storage.sync.set(cfg);
+			$('.debug').html("<h3>"+DecodedData.length+" Bookmarks imported</h3>");
+		});
+	});
 	if(data["Data"] && data["Data"].length > 0) {
 		for (i = 0; i < data["Data"].length; i++) { 
 			var DataURL = data["Data"][i]["VodTimestamp"];
@@ -20,10 +35,9 @@ $( document ).ready(function() {
 					chrome.storage.sync.set(cfg);
 				});
 		});
+	$(".BackupExport").append(btoa(JSON.stringify(data["Data"])));
 	} else {
 		$(".status").append('<div class="error">Aww...no bookmarks :(</div>');
 	}
 	});
-
-
 });
